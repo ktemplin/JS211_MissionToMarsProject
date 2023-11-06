d2de2f2fedc8378bb95c4a9c05eb93f56ff8c140
Kris Templin <kris.templin@gmail.com>
2023-11-06 13:21:25 -0800

    All unit tests passing

--- main.js
+++ main.js
@@ -10,11 +10,32 @@ const jobTypes = {
 };
 
 class CrewMember {
-
+  constructor(name, job, specialSkill, ship){
+    this.name = name
+    this.job = job
+    this.specialSkill = specialSkill
+    this.ship = ship
+  }
+  enterShip(ship){
+    this.ship = ship
+    this.ship.crew.push(this)
+  }
 }
 
 class Ship {
-
+  constructor(name, type, ability, crew){
+    this.name = name
+    this.type = type
+    this.ability = ability
+    this.crew = []
+  }
+  missionStatement() {
+    if (this.crew.length === 0) {
+      return "Can't perform a mission yet."
+    } else {
+      return this.ability
+    }
+  }
 }
 
 
